499b8167d0185284b5f3d188c58ad42758c144fc
Lucacel <razvanlucacel@gmail.com>
2018-05-20 16:39:01 +0300

    lab4_lastEdit

---
+++ 02-ProiectSalariati/ProiectSalariati/src/test/java/salariati/test/Integrare_Big_Bang.java
@@ -0,0 +1,60 @@
+package salariati.test;
+
+import org.junit.Before;
+import org.junit.Test;
+import salariati.controller.EmployeeController;
+import salariati.controller.UIController;
+import salariati.enumeration.DidacticFunction;
+import salariati.model.Employee;
+import salariati.repository.interfaces.EmployeeRepositoryInterface;
+import salariati.repository.mock.EmployeeMock;
+import salariati.validator.EmployeeValidator;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+public class Integrare_Big_Bang {
+    private UIController uiController;
+
+    @Before
+    public void setUp(){
+        EmployeeRepositoryInterface repositoryInterface = new EmployeeMock();
+        EmployeeController controller = new EmployeeController(repositoryInterface);
+        EmployeeValidator validator = new EmployeeValidator();
+        uiController = new UIController(controller, validator);
+    }
+
+    @Test
+    public void unit_test_A(){
+        String result;
+        result = uiController.addEmployee("LastName", "1960514135456", DidacticFunction.ASISTENT, "10");
+        assertEquals("Added: LastName;1960514135456;ASISTENT;10", result);
+    }
+
+    @Test
+    public void unit_test_B(){
+        String result;
+        result = uiController.modifyFunction("1960519152485", DidacticFunction.ASISTENT);
+        assertEquals("Employee modified",result);
+    }
+
+    @Test
+    public void unit_test_C(){
+        List<Employee> employees = uiController.getEmployeeBySalary();
+        for(int i = 0; i < employees.size() - 1; i++){
+            Employee e1 = employees.get(i);
+            Employee e2 = employees.get(i+1);
+            assertTrue(Integer.parseInt(e1.getSalary()) >= Integer.parseInt(e2.getSalary()));
+        }
+    }
+
+    @Test
+    public void unit_test_combined(){
+        unit_test_A();
+        unit_test_B();
+        unit_test_C();
+    }
+
+}
